31b74915e087b09172cd0da905df8011dea7fb42
Hongrui Fang <chfanghr@gmail.com>
2023-03-01 20:52:57 +0800

    add `verify` subcommand for validating effect schemas

--- agora-effect-registry-exe/Main.hs
+++ agora-effect-registry-exe/Main.hs
@@ -1,16 +1,44 @@
 module Main (main) where
 
-import AgoraRegistry.Server.EffectRegistry (loadEffects)
-import AgoraRegistry.Server.Options (Options (HttpServerOption), parseOptions)
+import AgoraRegistry.Schema (EffectSchema)
+import AgoraRegistry.Server.EffectRegistry (loadEffect, loadEffects)
+import AgoraRegistry.Server.Options (HttpServerOptions)
 import AgoraRegistry.Server.Server (runServer)
-
--- | Paths from which the server loads effect schemas.
-effectsDir :: FilePath
-effectsDir = "./effects"
+import Control.Monad (unless)
+import Control.Monad.Except (catchError)
+import Data.Aeson qualified as Aeson
+import Data.Foldable (traverse_)
+import Options (Commands (Serve, Verify), parseOptions)
 
 main :: IO ()
-main = do
-  HttpServerOption opts <- parseOptions
+main =
+  parseOptions >>= \case
+    Serve effectDir serverOpts -> serve effectDir serverOpts
+    Verify paths -> verify paths
+
+serve :: FilePath -> HttpServerOptions -> IO ()
+serve effectsDir serverOpts = do
   registry <- loadEffects effectsDir
   putStrLn "Effect schemas loaded."
-  runServer effectsDir registry opts
+  runServer effectsDir registry serverOpts
+
+verify :: [FilePath] -> IO ()
+verify =
+  traverse_
+    ( \path -> catchError (verifySingle path) $ \err ->
+        putStrLn $
+          "Error while verifying "
+            <> show path
+            <> ": "
+            <> show err
+    )
+  where
+    verifySingle :: FilePath -> IO ()
+    verifySingle path = do
+      schema <- loadEffect path
+
+      case Aeson.decode @EffectSchema $ Aeson.encode schema of
+        Nothing -> fail "round-trip failure"
+        Just schema' ->
+          unless (schema' == schema) $
+            fail "schema should be identical after round-trip"
---
+++ agora-effect-registry-test/CodeGenTests.hs
@@ -0,0 +1,2 @@
+module CodeGenTests() where
+
